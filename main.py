aa99fe0b5b552dc6d670922a94e2d18bf21fab69
Oregano-jpg <cadhu.01.0287@gmail.com>
2024-12-11 00:45:04 -0300

    vendas controle

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-input("insira um numero")
-
+rendimento = float(input("insira o rendimento mensal "))
+custo = float(input("insira o custo total "))
+novas_vendas = float(input("insira novas venad aqui caso nao digite 0"))
+imposto = rendimento *0.1
+resultado = rendimento-custo+novas_vendas 
+print("o resultado total esperado e de ",resultado)
