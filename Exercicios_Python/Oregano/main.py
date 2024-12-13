f9bc7ed3353a9d95768e47adc9799053eac51a4e
wallace-pv <wallace-pv>
2024-12-13 01:33:46 -0300

    Organizando pastas

--- Exercicios_Python/Oregano/main.py
+++ Exercicios_Python/Oregano/main.py
@@ -1,6 +1,6 @@
 rendimento = float(input("insira o rendimento mensal "))
 custo = float(input("insira o custo total "))
-novas_vendas = float(input("insira novas venad aqui caso nao digite 0"))
+novas_vendas = float(input("insira novas vendas aqui caso nao digite 0"))
 imposto = rendimento *0.1
 resultado = rendimento-custo+novas_vendas 
 print("o resultado total esperado e de ",resultado)
