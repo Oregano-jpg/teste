a2cf3ec4f2951a6a50f3dd46b88fcb760e738cd6
wallace-pv <wallace-pv@hotmail.com>
2024-12-13 08:20:08 -0300

    Adicionando verificaçao de palavra

--- Exercicios_Python/Wallace/ex_Palindromo.py
+++ Exercicios_Python/Wallace/ex_Palindromo.py
@@ -11,21 +11,32 @@
 
 
 #usando função para verificar se é um palindromo
-
+import re
+#criando um funcao para verificar se a entrada é valida
+def palavraValida(palavra):
+     # Aceita letras e espaços, desde que haja pelo menos uma letra
+    return re.match(r"^\s*[a-zA-Z]+(?:\s+[a-zA-Z]+)*\s*$", palavra)
+#essa funcao verifica se é um palindromo
 def verificarPalindromo(palavra):
-    palavra = palavra.lower().replace(" ","")
+    if not palavraValida(palavra):
+        return None
+    palavra = str(palavra).lower().replace(" ","")
     return palavra == palavra[::-1]
+    
 #criando outra funcao só para a parte de exibir a palavra ao contrario, ja que retornar um boleano na verificação é melhor que retornar print
 def exibirPalavraInvertida(palavra):
-    palavraInvertida = "".join(reversed((palavra)))
-    print(f"A palavra ao contrario fica {palavraInvertida}")
+    if palavraValida(palavra):
+        palavraInvertida = "".join(reversed((palavra)))
+        print(f"A palavra ao contrario fica {palavraInvertida}")
+    else:
+        print("Entrada inválida. Por favor, digite apenas letras e espaços.")
 
 ##chamando e usando as funções
 palavra = input("Digite a palavra que você queira verificar se é um palindromo: ")
 
 exibirPalavraInvertida(palavra)
-
-if verificarPalindromo(palavra):
+resultado = verificarPalindromo(palavra)
+if resultado is True:
     print(f"logo a palavra {palavra} é sim um palindromo")
-else:
+elif resultado is False:
     print(f"como a palavra {palavra} invertida não fica igual logo ela não é um palindromo")
