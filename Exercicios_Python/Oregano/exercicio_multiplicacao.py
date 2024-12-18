c8160b21f270d94c39b20845c908df248b5e4660
Oregano-jpg <cadhu.01.0287@gmail.com>
2024-12-18 02:54:29 -0300

    exercicios novos

---
+++ Exercicios_Python/Oregano/exercicio_multiplicacao.py
@@ -0,0 +1,5 @@
+soma_inicial =input (int("qual numero voce quer multiplicar ?"))
+soma_final = input (int("sera multiplicado por ?"))
+resultado = soma_inicial * soma_final
+print (resultado) 
+
---
+++ Exercicios_Python/Oregano/exercicio_tabuada.py
@@ -0,0 +1,4 @@
+numero = int(input("digite o numero que voce quer ver a tabuada ? \n"))
+for  i in range (11) :
+    resultado = i * numero
+    print (f"{numero}x{i}={resultado}")
